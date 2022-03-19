339d16849d1f93503f3d58d069dda7fcbb2f8721
evandros-ks <ekaklama@uwaterloo.ca>
2022-03-19 15:23:44 -0400

    parse inputs

--- convert_datasets.py
+++ convert_datasets.py
@@ -1,3 +1,4 @@
+import argparse
 import numpy as np
 import pandas as pd
 
@@ -25,3 +26,29 @@ def add_related_label(df: pandas.core.frame.DataFrame):
     '''Adds column to dataframe whether body is related to stance'''
     df['related'] = np.where(df['Stance']!= 'unrelated', True, False)
     return df
+
+
+def save_data(output: str, train_df: pandas.core.frame.DataFrame,
+              test_df: pandas.core.frame.DataFrame):
+    '''Save csv to output directory
+    
+    Args:
+        output (str): Output directory to save csv
+        train_df(pandas.core.frame.DataFrame): dataframe of training set
+        test_df(pandas.core.frame.DataFrame): dataframe of competition test set
+    '''
+    train_df.to_csv('train.csv', index=False, encoding='utf-8')
+
+    test_df.to_csv('test.csv', index=False, encoding='utf-8')
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-i", "--input-dir", help="path to input fake news challenge directory",
+                        action="store")
+    parser.add_argument("-o", "--output-dir", help="path to saved processed csvs",
+                        action="store")
+
+    args = parser.parse_args()
+    
+    main(input_dir=args.input_dir, output_dir=args.output_dir)
